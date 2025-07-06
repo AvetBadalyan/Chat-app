612c95013ce792a77adc9e5bd8da0d429a70a667
AvetBadalyan <avetbadalyan@gmail.com>
2025-07-06 16:36:19 +0400

    setup frontend with tailwind css, daisy UI and state with zustand

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import messageRoutes from './routes/message.route.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/db.js';
+import cors from 'cors';
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
@@ -12,6 +14,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+);
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
