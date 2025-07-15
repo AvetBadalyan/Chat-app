f04f951e106afa39e4b4de56ac52e0bc33f43d35
AvetBadalyan <avetbadalyan@gmail.com>
2025-07-16 01:08:14 +0400

    update packages

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,18 +6,14 @@ import cookieParser from 'cookie-parser';
 import { connectDB } from './lib/db.js';
 import cors from 'cors';
 import path from 'path';
-
 import { app, server } from './lib/socket.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-app.use(express.json());
-app.use(cookieParser());
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
+
 app.use(
   cors({
     origin: 'http://localhost:5173',
@@ -25,15 +21,23 @@ app.use(
   }),
 );
 
+app.use(express.json({ limit: '10mb' }));
+app.use(cookieParser());
+
+// API routes
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+
+// Serve static files in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
   });
 }
 
 server.listen(PORT, () => {
-  console.log('Server is running on port ' + PORT);
+  console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
