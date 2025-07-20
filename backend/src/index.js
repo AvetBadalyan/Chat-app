552764371098c9154d2c760dbd421878e9afe047
AvetBadalyan <avetbadalyan@gmail.com>
2025-07-20 15:34:19 +0400

    update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,11 +13,17 @@ dotenv.config();
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-app.use(express.json());
+
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? 'https://avetbook-chat-app.onrender.com'
+        : 'http://localhost:5173',
     credentials: true,
   }),
 );
@@ -27,7 +33,6 @@ app.use('/api/messages', messageRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
   });
