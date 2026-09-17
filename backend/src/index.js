7169582da48dfe1d48b8c89476ee70b8fc26055b
Avet Badalyan <avetbadalyan@gmail.com>
2026-09-17 15:11:43 +0400

    fly config

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(
 	cors({
 		origin:
 			process.env.NODE_ENV === 'production'
-				? 'https://avetbook-chat-app.onrender.com'
+				? 'https://chat-app-avetbook.fly.dev'
 				: 'http://localhost:5173',
 		credentials: true
 	})
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,7 +9,7 @@ const io = new Server(server, {
 	cors: {
 		origin:
 			process.env.NODE_ENV === 'production'
-				? ['https://avetbook-chat-app.onrender.com']
+				? 'https://chat-app-avetbook.fly.dev'
 				: ['http://localhost:5173'],
 		credentials: true
 	}
